2ca4e5b229a661e9f4ea759946351763798a0fc6
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-10 23:11:40 -0300

    Fix: render IMG on screen!

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-const Canvas = ({draw, height, width, image}) => {
+const Canvas = ({draw, height, width}) => {
   const canvas = React.useRef();
   React.useEffect(() => {
-      const ctx = canvas.current.getContext('2d');
-      const canvasAct = document.querySelector('canvas')
+  const ctx = canvas.current.getContext('2d');
+  // const ctx = this.refs.canvas.getContext('2d');
+  const canvasAct = document.querySelector('canvas')
 
     ctx.fillStyle = 'white';
     ctx.fillRect(0, 0, width, height) // fill the canvas with black color
     ctx.drawImage(canvasAct, 0, 0, width, height)
-    
     const image = new Image();
-    image.src = '../src/components/img/base.png';
+    image.src =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Picture_icon_BLACK.svg/1200px-Picture_icon_BLACK.svg.png";
     image.onload = () => {
-        ctx.drawImage(image, 0, 0);
-    }
-    console.log(canvasAct)
-    console.log(image)
+      ctx.drawImage(image, 0, 0, height, width);
+    };
+  
 
      draw(ctx);
 
-  });
+  }, [draw, height, width]);
+
 return (
     <canvas ref={canvas} height={height} width={width} />
   );
@@ -30,4 +31,5 @@ Canvas.propTypes = {
   height: PropTypes.number.isRequired,
   width: PropTypes.number.isRequired,
 };
+
 export default Canvas;
