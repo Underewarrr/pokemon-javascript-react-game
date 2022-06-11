6c1f16fffd6e55b5c3beb499e56cd42bc005325d
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-11 00:52:08 -0300

    Dev: playerWalk Lister for Arrow Keys

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -5,36 +5,58 @@ var centerY = 0;
 const Canvas = ({draw, height, width}) => {
   const canvas = React.useRef();
   React.useEffect(() => {
-  const ctx = canvas.current.getContext('2d');
+    const ctx = canvas.current.getContext('2d');
   // const ctx = this.refs.canvas.getContext('2d');
-  const canvasAct = document.querySelector('canvas')
-    ctx.fillStyle = 'white';
-    ctx.fillRect(centerX, centerY, width, height) // fill the canvas with black color
-    ctx.drawImage(canvasAct, centerX, centerY, width, height)
-  const gameScene = new Image();
-    gameScene.src =
+    const canvasAct = document.querySelector('canvas')
+      ctx.fillStyle = 'white';
+      ctx.fillRect(centerX, centerY, width, height) // fill the canvas with black color
+      ctx.drawImage(canvasAct, centerX, centerY, width, height)
+
+    const gameScene = new Image();
+      gameScene.src =
       "https://i.imgur.com/rkxlut8.png";
-  const playerImage = new Image()
-    playerImage.src =
-    "https://i.imgur.com/z7zrjm4.png";
+    const playerImage = new Image()
+      playerImage.src =
+      "https://i.imgur.com/z7zrjm4.png";
+
     gameScene.onload = () => {
-    ctx.drawImage(gameScene, 0, 0, width, height);
-    ctx.drawImage(playerImage,
+      ctx.drawImage(gameScene, 0, 0, width, height);
+      ctx.drawImage(playerImage,
       // START CROPPING SPRITE HERE
-      centerX, // X Coordinates
-      centerY, // Y Coordinates
-      playerImage.width / 4, // Width of the image
-      playerImage.height, // Height
+      centerX, 
+      centerY, 
+      playerImage.width / 4,
+      playerImage.height,
       // ACTUAL SPRITE SIZE
       width / 2 - (playerImage.width / 4) / 2, // Divide a sprite into 4 parts and take the middle one
       height / 2 - playerImage.height / 2,
-      playerImage.width / 4, // Width of the image
-      playerImage.height, // Height
+      playerImage.width / 4, 
+      playerImage.height,
       // END CROPPING SPRITE HERE
       );
     };
   draw(ctx);
 }, [draw, height, width]);
+
+// Walk 
+window.addEventListener('keydown', (playerWalk) => {
+  switch (playerWalk.key) {
+    case 'ArrowUp':
+      console.log('ArrowUp')
+    break
+    case 'ArrowDown':
+      console.log('ArrowDown')
+    break
+    case 'ArrowLeft':
+      console.log('ArrowLeft')
+    break
+    case 'ArrowRight':
+      console.log('ArrowRight')
+    break
+    default:
+    break
+  }
+})
 
 return (
     <canvas ref={canvas} height={height} width={width} />
