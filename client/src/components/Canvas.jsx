540c16d313ac760a7835fddb5473632a264520ad
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-12 18:55:33 -0300

    Fix : Player movement infinit loop

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 var centerX = 0;
 var centerY = 0;
+var spriteSize = 4;
+var spriteMiddle = 2;
 
 document.addEventListener('keydown', function(playerWalk) {
   switch (playerWalk.key) {
@@ -23,22 +25,7 @@ document.addEventListener('keydown', function(playerWalk) {
   }
 });
 
-const Canvas = ({draw, height, width}) => {
-   /* const [keys, setKeys] = useState({
-    ArrowUp: {
-      pressed: false,
-    },
-    ArrowDown: {
-      pressed: false,
-    },
-    ArrowRight: {
-      pressed: false,
-    },
-    ArrowLeft: {
-      pressed: false,
-    }
-  })  */
-
+const Canvas = ({draw, height, width}) => {  
 const canvas = React.useRef();
   React.useEffect(() => {
     const ctx = canvas.current.getContext('2d');
@@ -63,20 +50,21 @@ const gameSceneLayer = new Sprite({
   },
   image: gameScene
 })
-gameSceneLayer.draw()
 
-  function animate() {
+
+function animate() {
+    gameSceneLayer.draw()
     window.requestAnimationFrame(animate);
     ctx.drawImage(playerImage,
     // START CROPPING SPRITE HERE
     centerX, 
     centerY, 
-    playerImage.width / 4,
+    playerImage.width / spriteSize,
     playerImage.height,
     // ACTUAL SPRITE SIZE
-    width / 2 - (playerImage.width / 4) / 2, // Divide a sprite into 4 parts and take the middle one
-    height / 2 - playerImage.height / 2,
-    playerImage.width / 4, 
+    width / spriteMiddle - (playerImage.width / spriteSize) / spriteMiddle,
+    height / spriteMiddle - playerImage.height / spriteMiddle,
+    playerImage.width / spriteSize, 
     playerImage.height,
     // END CROPPING SPRITE HERE
     );   
