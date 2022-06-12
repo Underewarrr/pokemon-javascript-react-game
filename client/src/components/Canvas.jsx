a42a5dc4e6c1497ada8409fabffd1b90a044c3fa
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-06-12 19:27:02 -0300

    Dev: Player walking :100:

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -6,19 +6,61 @@ var centerY = 0;
 var spriteSize = 4;
 var spriteMiddle = 2;
 
+
+const keys = {
+  ArrowUp: {
+    pressed:false
+  },
+  ArrowDown: {
+    pressed:false
+  },
+  ArrowLeft: {
+    pressed:false
+  },
+  ArrowRight: {
+    pressed:false
+  }
+}
+
 document.addEventListener('keydown', function(playerWalk) {
   switch (playerWalk.key) {
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = true
+      console.log('Walk Up')
+    break;
+    case 'ArrowDown':
+      keys.ArrowDown.pressed = true
+      console.log('Walk Down')
+    break;
       case 'ArrowLeft':
+        keys.ArrowLeft.pressed = true
         console.log('Walk Left')
       break;
       case 'ArrowRight':
+        keys.ArrowRight.pressed = true
         console.log('Walk Right')
       break;
-      case 'ArrowUp':
-        console.log('Walk Up')
+      default:
       break;
-      case 'ArrowDown':
-        console.log('Walk Down')
+  }
+});
+document.addEventListener('keyup', function(playerWalk) {
+  switch (playerWalk.key) {
+    case 'ArrowUp':
+      keys.ArrowUp.pressed = false
+      console.log('Walk Up')
+    break;
+    case 'ArrowDown':
+      keys.ArrowDown.pressed = false
+      console.log('Walk Down')
+    break;
+      case 'ArrowLeft':
+        keys.ArrowLeft.pressed = false
+        console.log('Walk Left')
+      break;
+      case 'ArrowRight':
+        keys.ArrowRight.pressed = false
+        console.log('Walk Right')
       break;
       default:
       break;
@@ -30,30 +72,30 @@ const canvas = React.useRef();
   React.useEffect(() => {
     const ctx = canvas.current.getContext('2d');
     const playerImage = new Image()
-      playerImage.src =
-      "https://i.imgur.com/z7zrjm4.png";
-class Sprite {
-  constructor({ position, velocity, image }) {
-    this.position = position
-    this.image = image
-  }
-  draw(){
-    ctx.drawImage(this.image, centerX, centerY, width, height);
-  }
-}
-const gameScene = new Image();
-gameScene.src = "https://i.imgur.com/rkxlut8.png";
-const gameSceneLayer = new Sprite({   
-  position: { 
-    x: centerX,
-    y: centerY
-  },
-  image: gameScene
-})
+      playerImage.src = "https://i.imgur.com/z7zrjm4.png";
+  class Sprite {
+    constructor({ position, velocity, image }) {
+      this.position = position
+      this.image = image 
+    }
+  
+    draw(){
+      ctx.drawImage(this.image, this.position.x, this.position.y, width, height);
+    }}
+// Game Scene Configuration
+  const gameScene = new Image();
+    gameScene.src = "https://i.imgur.com/rkxlut8.png";
+  const gameSceneLayer = new Sprite({   
+    position: { 
+      x: centerX,
+      y: centerY
+    },
+    image: gameScene
+}) //  End Game Scene Configuration
 
 
 function animate() {
-    gameSceneLayer.draw()
+    gameSceneLayer.draw() // Draw Game Scene Layer
     window.requestAnimationFrame(animate);
     ctx.drawImage(playerImage,
     // START CROPPING SPRITE HERE
@@ -68,6 +110,9 @@ function animate() {
     playerImage.height,
     // END CROPPING SPRITE HERE
     );   
+    if (keys.ArrowUp.pressed) {
+      gameSceneLayer.position.y = gameSceneLayer.position.y - 3;
+    }
   }
   animate();
 }, [draw, height, width]);
