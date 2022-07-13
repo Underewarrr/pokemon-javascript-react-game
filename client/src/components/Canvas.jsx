b194ba88590368d88d166f2fffaa7378f3a0866a
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-13 10:09:20 -0300

    Dev: Track last key pressed

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -21,22 +21,27 @@ const keys = {
   }
 }
 
+let lastKeyDown = '';
 document.addEventListener('keydown', function(playerWalk) {
   switch (playerWalk.key) {
     case 'ArrowUp':
       keys.ArrowUp.pressed = true
+      lastKeyDown = 'ArrowUp'
       console.log('Walk Up')
     break;
     case 'ArrowDown':
       keys.ArrowDown.pressed = true
+      lastKeyDown = 'ArrowDown'
       console.log('Walk Down')
     break;
       case 'ArrowLeft':
         keys.ArrowLeft.pressed = true
+        lastKeyDown = 'ArrowLeft'
         console.log('Walk Left')
       break;
       case 'ArrowRight':
         keys.ArrowRight.pressed = true
+        lastKeyDown = 'ArrowRight'
         console.log('Walk Right')
       break;
       default:
@@ -122,6 +127,7 @@ function animate() {
       gameSceneLayer.position.x = gameSceneLayer.position.x + 3;  // Move Right
     }
   }
+  
   animate();
 }, [draw, height, width]);
 
