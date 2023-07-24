238391d14e7e1f46309e65509ae7f93baa69bf5f
Underewarrr <rafhaelxd@gmail.com>
2023-07-24 15:43:31 -0300

    Dev: Dynamic Tile Map Rendering for 2D Game
    We implemented dynamic tile map rendering for our 2D game. We introduced a TILE_TYPES object that defines different types of tiles along with their corresponding colors. The drawTile function was updated to use this object to render tiles on the canvas based on the provided tile map data.
    
    To showcase this feature, we created a new tile map representing a grass area with a river. The map is loaded and rendered using the Canvas component, which also includes the player and their movement logic.
    
    This dynamic tile map rendering enables us to create diverse and customizable game worlds, making our 2D game more engaging and immersive. The commit lays the foundation for further expansion of the game world with different tile types, adding depth and complexity to the gameplay experience.
    
    The addition of dynamic map loading and rendering will greatly enhance the gameplay and level design possibilities of our 2D game.
    This marks an exciting milestone in the development of our project and sets the stage for further advancements in the game's environment and mechanics.

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -1,29 +1,41 @@
+// Canvas.jsx
+
 import React, { useEffect, useRef, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import { updatePlayerPosition } from '../game/control/player'; 
-import { drawGameWorld } from '../game/scenes/world'; 
+import { drawGameWorld, drawTile } from '../game/scenes/world'; // Import the new drawTile function
 import { drawPlayer, playerImgUrl } from '../game/scenes/player'; 
-import { animate } from '../game/animations/animate'; // Import the animate function from the external file
-
+import { animate } from '../game/animations/animate';
+import { TILE_TYPES } from '../game/items/tiles/types'; // Import the TILE_TYPES object
 
 const Canvas = ({ draw, height, width, spriteSize = 4, spriteMiddle = 2 }) => {
   const canvasRef = useRef();
   const playerImageRef = useRef(new Image());
   playerImageRef.current.src = playerImgUrl;
-  const playerPosition = useMemo(() => ({ x: width / 2, y: height / 2 }), [height, width]); // Initial player position
+  const playerPosition = useMemo(() => ({ x: width / 2, y: height / 2 }), [height, width]);
+
+  // Tile map data
+  const initialTileMap = [
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WOOD',  'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],  
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+    ['GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'WATER', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS', 'GRASS'],
+  ];
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
-
-    const cleanup = animate(ctx, drawGameWorld, updatePlayerPosition, drawPlayer, width, height, playerPosition, playerImageRef, spriteSize);
-
+    const cleanup = animate(ctx, () => drawGameWorld(ctx, width, height, initialTileMap), updatePlayerPosition, drawPlayer, width, height, playerPosition, playerImageRef, spriteSize);
     return () => {
       cleanup();
     };
-  }, [draw, height, width, playerPosition, playerImageRef, spriteSize]);
-
-  // ...
+  }, [draw, height, width, playerPosition, playerImageRef, spriteSize, initialTileMap]);
 
   return <canvas ref={canvasRef} height={height} width={width} />;
 };
